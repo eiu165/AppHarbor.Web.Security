6ef70801070c734ff2864e6809216af9db73c7e1
nick <eiu165@gmail.com>
2013-05-19 22:49:07 -0500

    working with migration

---
+++ Auth.Data/PersistenceSupport/SqlUserRepository.cs
@@ -0,0 +1,44 @@
+﻿using Auth.Enitity;
+using Massive;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Auth.Data.PersistenceSupport
+{
+
+    public class SqlUserRepository : IRepository
+    {
+        private readonly DynamicModel _membershipTable;
+
+        public SqlUserRepository (string connectionString)
+        {
+            this._membershipTable = new DynamicModel(connectionString, "[dbo].[Membership]", "Id");
+        }
+
+
+        public T Get<T>(Guid id) where T : Entity
+        {
+            throw new NotImplementedException();
+            //IEnumerable<dynamic> data = this._membershipTable.Query("SELECT top 1 * FROM Membership"); ; 
+            //return data.Select( x => new User
+            //        {
+            //            Id = x.Id,
+            //            Username = x.Name,
+            //            Password = x.Password 
+            //        }).FirstOrDefault<T>(); 
+        }
+
+        public IQueryable<T> GetAll<T>() where T : Entity
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveOrUpdate<T>(T entity) where T : Entity
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
---
+++ Auth.SolutionItem/Data/Migration/Local/Reset.bat
@@ -0,0 +1,15 @@
+﻿
+
+
+ 
+@echo off
+cls
+"C:\Program Files\Microsoft SQL Server\110\Tools\Binn\sqlcmd" -E -S .\sqlexpress -i ../Local/DropDb.sql
+"C:\Program Files\Microsoft SQL Server\110\Tools\Binn\sqlcmd" -E -S .\sqlexpress -i ../2013-05-19/Migration.sql  
+
+pause
+
+
+
+
+
