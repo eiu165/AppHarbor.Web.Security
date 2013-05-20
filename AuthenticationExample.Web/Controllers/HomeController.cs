c8e3477798c7b035cc98e11c351eac7b0639339e
nick <eiu165@gmail.com>
2013-05-19 21:00:05 -0500

    added entity proj

--- AuthenticationExample.Web/Controllers/HomeController.cs
+++ AuthenticationExample.Web/Controllers/HomeController.cs
@@ -1,11 +1,12 @@
-﻿using System.Web.Mvc;
+﻿using System.Web.Mvc; 
+
 
 namespace AuthenticationExample.Web.Controllers
 {
 	public class HomeController : Controller
 	{
 		public ActionResult Index()
-		{
+		{ 
 			return View();
 		}
 	}
